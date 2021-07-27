47b6751cf28be07e24ff8783344b53408b901647
Ichinga Samuel <ichingasamuel@gmail.com>
2021-07-27 23:39:17 +0100

    .

--- models/Users.js
+++ models/Users.js
@@ -43,18 +43,6 @@ UserSchema.virtual('fave', {
 
 const User = mongoose.model('Users', UserSchema);
 
-// Create Admin Account when running for the first time
-// User.findOne({email: '************'}, (err, user) => {
-//     if(user){console.log('Admin has been created')}
-//     else{
-//         let pwd = "****************";
-//         pwd = genPwd(pwd);
-//         User.create({name: 'Ichinga Samuel', email: 'ichingasamuel@gmail.com', password: pwd, admin: true})
-//             .then(() => console.log('Admin Account Created'))
-//             .catch(console.error);
-//     }
-// });
-
 module.exports = User;
 
 
